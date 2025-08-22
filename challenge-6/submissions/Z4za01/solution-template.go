88877fc7be5e1ff1ed84f724bbd5dbb672c5a29b
Z4za01 <ugughr63@gmail.com>
2025-08-22 12:04:04 +0800

    Add solution for Challenge 6 by Z4za01

---
+++ challenge-6/submissions/Z4za01/solution-template.go
@@ -0,0 +1,43 @@
+// Package challenge6 contains the solution for Challenge 6.
+package challenge6
+
+import (
+	// Add any necessary imports here
+    "strings"
+	"unicode"
+)
+
+// CountWordFrequency takes a string containing multiple words and returns
+// a map where each key is a word and the value is the number of times that
+// word appears in the string. The comparison is case-insensitive.
+//
+// Words are defined as sequences of letters and digits.
+// All words are converted to lowercase before counting.
+// All punctuation, spaces, and other non-alphanumeric characters are ignored.
+//
+// For example:
+// Input: "The quick brown fox jumps over the lazy dog."
+// Output: map[string]int{"the": 2, "quick": 1, "brown": 1, "fox": 1, "jumps": 1, "over": 1, "lazy": 1, "dog": 1}
+func CountWordFrequency(text string) map[string]int {
+	// Your implementation here
+	res := make(map[string]int)
+	var b strings.Builder
+
+	for _, r := range text {
+		switch {
+		case unicode.IsLetter(r) || unicode.IsDigit(r):
+			b.WriteRune(unicode.ToLower(r))
+		case r == '\'' || r == '’':
+		default:
+
+			if b.Len() > 0 {
+				res[b.String()]++
+				b.Reset()
+			}
+		}
+	}
+	if b.Len() > 0 {
+		res[b.String()]++
+	}
+	return res
+} 
